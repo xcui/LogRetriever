c3642d413c6ea867f5d31d69a568e80b8015e3ef
Xiao <cuixiaokop@gmail.com>
2024-05-20 21:53:59 -0700

    Create a hello world project for Restful API

---
+++ src/test/java/com/example/logretriever/LogRetrieverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.logretriever;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogRetrieverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
